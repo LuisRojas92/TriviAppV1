8952b226009957235d6e980740c99dee4e7726b7
Luis Rojas <143117883+LuisRojas92@users.noreply.github.com>
2024-11-20 13:06:10 -0600

    Add files via upload
    V1 TriviApp de 10 preguntas  utilizando 
    Html
    CSS
    JavaScript 
    Uso de API's 
    
    Falta mejorar diseño y mas intuitivo asi como mejorar el lugar de aparicion del puntaje final de la trivia, esperando pronto traer la version en español de temas interesantes.

---
+++ main.js
@@ -0,0 +1,130 @@
+const botonGenerar = document.querySelector("#botonGenerar");
+const dificultad = document.querySelector("#dificultad");
+const respuesta = document.querySelector("#respuesta");
+const categoria = document.querySelector("#categoria");
+const trivia = document.querySelector("#trivia");
+const calificacionAviso = document.querySelector("#calificacionAviso");
+let numeroPreguntas = 0;
+let calificacion = 0;
+
+botonGenerar.addEventListener("click", (e) => {
+  //Eliminar calificación si es que existe alguna previa///
+
+  calificacionAviso.innerHTML = "";
+  calificacionAviso.style.display = "none";
+
+  let api = "https://opentdb.com/api.php?amount=10";
+
+  if (dificultad.value != "") {
+    api += "&difficulty=" + dificultad.value;
+  }
+
+  if (respuesta.value != "") {
+    api += "&type=" + respuesta.value;
+  }
+
+  if (categoria.value != "") {
+    api += "&category=" + categoria.value;
+  }
+
+  llamarApi(api);
+});
+
+async function llamarApi(cadena) {
+  let api = cadena;
+  const resultado = await fetch(api);
+  const resultado2 = await resultado.json();
+  calificacion = 0;
+  numeroPreguntas = 0;
+
+  //Generamos la trivia
+  imprimirPreguntas(resultado2.results);
+  //console.log(resultado2);
+
+  //Agregamos funcionalidad a las respuestas
+  let botones = document.querySelectorAll(".respuesta");
+
+  botones.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      element.style.fontSize = "30px";
+      numeroPreguntas++;
+      if (element.getAttribute("data") == 1) {
+        calificacion++;
+
+        bloquearPregunta(element.parentNode.parentNode, "correcto");
+        colorearPregunta(element.parentNode.parentNode, "correcto");
+      } else {
+        bloquearPregunta(element.parentNode.parentNode, "incorrecto");
+        colorearPregunta(element.parentNode.parentNode, "incorrecto");
+      }
+
+      calificar();
+    });
+  });
+
+  //Checamos si ya terminó el juego y, de ser así, se imprime su calificación
+}
+
+function imprimirPreguntas(arreglo) {
+  let preguntas = arreglo;
+  let contador = 0;
+  const cadena = preguntas.map((pregunta) => {
+    let respuestas = [];
+    //Almacenamos las respuestas
+    //Vamos a ponerle a las respuestas incorrectas un identificador '0'
+
+    for (let x = 0; x < pregunta.incorrect_answers.length; x++) {
+      respuestas.push([0, pregunta.incorrect_answers[x]]);
+    }
+
+    //Agregamos la respuesta correcta a las incorrectas
+    respuestas.push([1, pregunta.correct_answer]);
+    //Revolvemos un array
+    respuestas.sort(function () {
+      return Math.random() - 0.5;
+    });
+    //Volvemos cadena de respuestas
+    respuestas = imprimirRespuestas(respuestas);
+    respuestas = respuestas.join(" ");
+    let cadena2 = `<div class='preguntaContenedor' data='${contador}'>
+            <div class='pregunta'>
+                ${contador + 1}.- ${pregunta.question}
+            </div>
+            <div class='respuestas'>
+                ${respuestas}
+            </div>    
+        </div>`;
+    contador++;
+    return cadena2;
+  });
+  trivia.innerHTML = cadena;
+}
+
+function imprimirRespuestas(respuestas) {
+  const respuestas2 = respuestas.map((respuesta) => {
+    return `<button class='btn btn-success respuesta' data='${respuesta[0]}'>${respuesta[1]}</span>`;
+  });
+  return respuestas2;
+}
+
+function bloquearPregunta(elemento, cadena) {
+  let hijos = elemento.childNodes[3].childNodes;
+  hijos.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+function colorearPregunta(elemento, cadena) {
+  cadena == "correcto"
+    ? (elemento.style.backgroundColor = "#D8EFC9")
+    : (elemento.style.backgroundColor = "#F8C4B5");
+}
+
+function calificar() {
+  if (numeroPreguntas == 10) {
+    alert("Su calificación es de " + calificacion + "/" + numeroPreguntas);
+    calificacionAviso.innerHTML =
+      "Su calificación es de " + calificacion + "/" + numeroPreguntas;
+    calificacionAviso.style.display = "";
+  }
+}
